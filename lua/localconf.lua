6815345cc451b1d7f871b8f91b797b07688e0117
zeureka <ranzihao0504@outlook.com>
2023-04-20 15:03:11 +0800

    chore: modify a tab length of 2 spaces

--- lua/localconf.lua
+++ lua/localconf.lua
@@ -1,7 +1,7 @@
 vim.o.relativenumber = true
 vim.o.number = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.o.smartindent = true
 vim.o.termguicolors = true
 vim.o.cursorline = true
@@ -11,6 +11,7 @@ vim.o.expandtab = true
 vim.o.wildmenu = true
 vim.o.ignorecase = true
 vim.o.swapfile = false
+vim.o.wrap = false
 -- Allow corsor to cross line boundaries.
 vim.o.whichwrap = vim.o.whichwrap .. ',<,>,h,l'
 
@@ -23,12 +24,12 @@ vim.cmd.colorscheme('tokyonight-moon')
 
 -- vim.cmd([[ autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]])
 vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = { "*" },
-    callback = function()
-        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
-            vim.api.nvim_exec("normal! g'\"", false)
-        end
+  pattern = { "*" },
+  callback = function()
+    if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.api.nvim_exec("normal! g'\"", false)
     end
+  end
 })
 
 -- Set completeopt to have a better completion experience
